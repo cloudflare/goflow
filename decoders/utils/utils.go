35ec5eb5cce711f1a6b968898b4f51045d99d8ab
Batyrkhan Koshenov <b.koshenov@kazdream.kz>
2022-10-31 13:19:43 +0600

    decoders/utils.go: add ReadUint16/32 funcs to avoid allocs when reading from buffer
    Signed-off-by: Batyrkhan Koshenov <b.koshenov@kazdream.kz>

--- decoders/utils/utils.go
+++ decoders/utils/utils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"bytes"
 	"encoding/binary"
 	"io"
 )
@@ -13,4 +14,42 @@ func BinaryDecoder(payload io.Reader, dests ...interface{}) error {
 		}
 	}
 	return nil
+}
+
+// ReadUint16FromBuffer reads Uint16 value from buffer and returns
+// boolean flag telling if it was a success.
+//
+// Value is treated as big endian.
+func ReadUint16FromBuffer(b *bytes.Buffer, x *uint16) bool {
+	var buf [2]byte
+
+	for i := range buf {
+		bt, err := b.ReadByte()
+		if err != nil {
+			return false
+		}
+		buf[i] = bt
+	}
+
+	*x = binary.BigEndian.Uint16(buf[:])
+	return true
+}
+
+// ReadUint32FromBuffer reads Uint32 value from buffer and returns
+// boolean flag telling if it was a success.
+//
+// Value is treated as big endian.
+func ReadUint32FromBuffer(b *bytes.Buffer, x *uint32) bool {
+	var buf [4]byte
+
+	for i := range buf {
+		bt, err := b.ReadByte()
+		if err != nil {
+			return false
+		}
+		buf[i] = bt
+	}
+
+	*x = binary.BigEndian.Uint32(buf[:])
+	return true
 }
